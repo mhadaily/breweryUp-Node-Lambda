fa1cc80a234a58b1b4e849a70225edcc62150657
Majid <mhadaily@gmail.com>
2017-02-12 15:55:05 +0800

    get all award events for a beer

---
+++ handlers/beers/events/getEvents.js
@@ -0,0 +1,22 @@
+/*
+ GET: /beer/:beerId/events
+ Gets a listing of all events that a beer is at or has won awards at.
+ */
+
+require('dotenv').config();
+const axios = require('axios');
+const {beerEndPoint, beersEndPoint, key} = require('../../endpoint');
+
+module.exports.getAwardEvents = (event, context, callback) => {
+
+  const beerID = event.path.id;
+
+  axios.get(`${beerEndPoint}/${beerID}/events`, {
+        params: {
+          key,
+          format: 'json'
+        }
+      })
+      .then(response => callback(null, response.data))
+      .catch(e => callback(e, null));
+};
